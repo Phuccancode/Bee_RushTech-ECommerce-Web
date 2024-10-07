639011428fe6964b625b3eaf1839d6a5335de6fc
Phuc Tran Hong <tranhongphucfb@gmail.com>
2024-10-07 21:37:01 +0700

    init

---
+++ sever/Bee_RushTech/src/main/java/com/project/Bee_RushTech/BeeRushTechApplication.java
@@ -0,0 +1,13 @@
+package com.project.Bee_RushTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeRushTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeRushTechApplication.class, args);
+	}
+
+}
