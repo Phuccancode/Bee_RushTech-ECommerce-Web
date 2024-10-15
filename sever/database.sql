97b4cb8adc073fa1ad328657eeff5ee74f40f7a7
Phuc Tran Hong <tranhongphucfb@gmail.com>
2024-10-15 12:11:20 +0700

    update db

--- sever/database.sql
+++ sever/database.sql
@@ -1,15 +1,16 @@
+
 CREATE DATABASE shopapp;
 USE shopapp;
---Khách hàng khi muốn mua hàng => phải đăng ký tài khoản => bảng users
+-- Khách hàng khi muốn mua hàng => phải đăng ký tài khoản => bảng users
 CREATE TABLE roles(
     id INT PRIMARY KEY,
     name VARCHAR(20) NOT NULL 
 );
 
 CREATE TABLE users(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id CHAR(10) PRIMARY KEY,
     fullname VARCHAR(100) DEFAULT '',
-    phone_number VARCHAR(10) NOT NULL,
+    phone_number CHAR(10) NOT NULL,
     address VARCHAR(200) DEFAULT '',
     password VARCHAR(100) NOT NULL DEFAULT '',
     created_at DATETIME,
@@ -19,81 +20,85 @@ CREATE TABLE users(
     facebook_account_id INT DEFAULT 0,
     google_account_id INT DEFAULT 0,
     role_id INT,
-    ADD FOREIGN KEY (role_id) REFERENCES roles (id)
+    FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
 
 
 CREATE TABLE tokens(
-    id int PRIMARY KEY AUTO_INCREMENT,
-    token varchar(255) UNIQUE NOT NULL,
-    token_type varchar(50) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    token_type VARCHAR(50) NOT NULL,
     expiration_date DATETIME,
-    revoked tinyint(1) NOT NULL,
-    expired tinyint(1) NOT NULL,
-    user_id int,
+    revoked TINYINT(1) NOT NULL,
+    expired TINYINT(1) NOT NULL,
+    user_id CHAR(10),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
---hỗ trợ đăng nhập từ Facebook và Google
+-- hỗ trợ đăng nhập từ Facebook và Google
 CREATE TABLE social_accounts(
     id INT PRIMARY KEY AUTO_INCREMENT,
     provider VARCHAR(20) NOT NULL COMMENT 'Tên nhà social network',
-    provider_id VARCHAR(50) NOT NULL,
+    provider_id CHAR(50) NOT NULL,
     email VARCHAR(150) NOT NULL COMMENT 'Email tài khoản',
     name VARCHAR(100) NOT NULL COMMENT 'Tên người dùng',
-    user_id int,
+    user_id CHAR(10),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
---Bảng danh mục sản phẩm(Category)
+-- Bảng danh mục sản phẩm(Category)
 CREATE TABLE categories(
     id INT PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) NOT NULL DEFAULT '' COMMENT 'Tên danh mục, vd: đồ điện tử'
 );
 
---Bảng chứa sản phẩm(Product): "laptop macbook air 15 inch 2023", iphone 15 pro,...
+-- Bảng chứa sản phẩm(Product): "laptop macbook air 15 inch 2023", iphone 15 pro,...
 CREATE TABLE products (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(350) COMMENT 'Tên sản phẩm',
+    brand VARCHAR(50) NOT NULL,
+    import_price FLOAT NOT NULL CHECK(import_price>=0),
     price FLOAT NOT NULL CHECK (price >= 0),
     thumbnail VARCHAR(300) DEFAULT '',
-    description LONGTEXT DEFAULT '',
+    description LONGTEXT,
     created_at DATETIME,
     updated_at DATETIME,
     category_id INT,
+    available TINYINT(1) DEFAULT 1,
+    color VARCHAR(20) DEFAULT '',
     FOREIGN KEY (category_id) REFERENCES categories (id)
 );
---Đặt hàng - orders
+-- Đặt hàng - orders
 CREATE TABLE orders(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id int,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    user_id CHAR(10),
     fullname VARCHAR(100) DEFAULT '',
     email VARCHAR(100) DEFAULT '',
-    phone_number VARCHAR(20) NOT NULL,
-    address VARCHAR(200) NOT NULL,
+    phone_number CHAR(10) NOT NULL,
     note VARCHAR(100) DEFAULT '',
     order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20)  ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled') COMMENT 'Trạng thái đơn hàng',
+    status ENUM('pending', 'processing', 'shipped', 'delivered', 'cancelled') COMMENT 'Trạng thái đơn hàng',
     total_money FLOAT CHECK(total_money >= 0),
-    shipping_method VARCHAR(100),
-    shipping_address VARCHAR(200),
-    shipping_date DATE,
-    tracking_number VARCHAR(100),
-    payment_method VARCHAR(100),
-    active TINYINT(1)
+    shipping_method VARCHAR(100) NOT NULL,
+    shipping_address VARCHAR(200) NOT NULL,
+    shipping_date DATE NOT NULL,
+    tracking_number VARCHAR(100) NOT NULL,
+    payment_method VARCHAR(100) NOT NULL,
+    active TINYINT(1),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 
 CREATE TABLE order_details(
     id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
-    FOREIGN KEY (order_id) REFERENCES orders (id),
     product_id INT,
-    FOREIGN KEY (product_id) REFERENCES products (id),
     price FLOAT CHECK(price >= 0),
     number_of_products INT CHECK(number_of_products > 0),
     total_money FLOAT CHECK(total_money >= 0),
-    color VARCHAR(20) DEFAULT ''
+	return_date DATE NOT NULL,
+    return_method ENUM('home', 'store'),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id)
 );
