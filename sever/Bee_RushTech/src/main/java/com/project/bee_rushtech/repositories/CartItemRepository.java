142e75d8a5b7fbe44180c5f9dc83eee398a709c2
dlpduy <dinhlephucduy@gmail.com>
2024-11-11 00:30:25 +0700

    finish basic add, update, remove item to cart

--- sever/Bee_RushTech/src/main/java/com/project/bee_rushtech/repositories/CartItemRepository.java
+++ sever/Bee_RushTech/src/main/java/com/project/bee_rushtech/repositories/CartItemRepository.java
@@ -5,5 +5,9 @@ import com.project.bee_rushtech.models.CartItem;
 import java.util.List;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findAllById(Long id); // Phương thức để tìm các mục giỏ hàng theo userId
+    List<CartItem> findAllByCartId(Long cartId); // Phương thức để tìm các mục giỏ hàng theo
+
+    CartItem findByIdAndCartId(Long id, Long cartId); // Phương thức để tìm mục giỏ hàng theo id và cartId
+
+    CartItem save(CartItem cartItem); // Phương thức để lưu mục giỏ hàng
 }
