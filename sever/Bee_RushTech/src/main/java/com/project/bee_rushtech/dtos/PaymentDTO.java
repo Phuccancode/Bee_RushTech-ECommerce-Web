3349f77726aa48b8d940d619d5cb748233f04940
Phuc Tran Hong <tranhongphucfb@gmail.com>
2024-11-14 18:08:57 +0700

    done payment soon

---
+++ sever/Bee_RushTech/src/main/java/com/project/bee_rushtech/dtos/PaymentDTO.java
@@ -0,0 +1,15 @@
+package com.project.bee_rushtech.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+public abstract class PaymentDTO {
+    @Builder
+    @AllArgsConstructor
+    public static class VNPayResponse {
+        public String code;
+        public String message;
+        public String paymentUrl;
+    }
+}
